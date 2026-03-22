e30d3b69c046b572617cbdfa212a7c90315f0d60
kimda <kimda@KDHAN>
2026-03-22 19:52:10 +0900

    초기 커밋.

---
+++ Revit2023Addin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit2023Addin
+{
+    public class Class1
+    {
+    }
+}
